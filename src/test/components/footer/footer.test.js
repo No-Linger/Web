1d87a77e621615dc556468582bfdfa668c6f54b3
m3llad0 <a01655451@tec.mx>
2023-10-12 17:17:32 -0600

    created tests

---
+++ src/test/components/footer/footer.test.js
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Footer from '@/app/components/footer/footer';
+
+describe('Footer', () => {
+  it('renders the footer text', () => {
+    const { getByTestId } = render(<Footer />);
+    const footerText = getByTestId('footer-text');
+    expect(footerText).toBeInTheDocument();
+  });
+
+  it('renders the footer link', () => {
+    const { getByText } = render(<Footer />);
+    const footerLink = getByText(/No-Linger™/i);
+    expect(footerLink).toHaveAttribute('href', 'https://github.com/No-Linger');
+  });
+});
---
+++ src/test/pages/login/login.test.js
@@ -0,0 +1,20 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Login from '@/app/login/page';
+
+describe('Login', () => {
+  it('renders the login form', () => {
+    render(<Login />);
+    const emailInput = screen.getByLabelText(/Correo/i);
+    const passwordInput = screen.getByLabelText(/Contraseña/i);
+    const rememberCheckbox = screen.getByLabelText(/Recuerdame/i);
+    const submitButton = screen.getByRole('button', { name: /Iniciar sesión/i });
+    const createAccountLink = screen.getByRole('link', { name: /Crear cuenta/i });
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(rememberCheckbox).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+    expect(createAccountLink).toBeInTheDocument();
+  });
+});
