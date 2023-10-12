aeaa434428b20087a691173e248d0de4b4b2c4fc
AlanSaid1 <99698053+AlanSaid1@users.noreply.github.com>
2023-10-12 16:40:57 -0600

    Login completo

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -5,7 +5,7 @@ return (
     <div className="w-full max-w-screen-xl mx-auto">
         <div className="sm:flex sm:items-center sm:justify-between">
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700"/>
+        <hr className="my-4 border-gray-200 sm:mx-auto dark:border-gray-700"/>
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://github.com/No-Linger" className="hover:underline">No-Linger™. </a>Todos los derechos reservados.</span>
     </div>
 </footer>
