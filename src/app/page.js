88061cf2392da75482af246b53af5fe86c36b7a1
m3llad0 <a01655451@tec.mx>
2023-10-15 00:09:33 -0600

    dashboard home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,14 @@
 "use client";
 import Navbar from "./components/navbar/navbar";
-export default function Example() {
+export default function Home() {
   return (
     <>
       <div className="min-h-screen bg-gray-100 dark:bg-gray-700">
         <Navbar />
-        <header className="bg-white dark:bg-gray-900 shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-300">Dashboard</h1>
-          </div>
-        </header>
         <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-300">Dashboard principal :)</h1>
+          </div>
         </main>
       </div>
     </>
