c9e1abb5e3d061290e3f7f2f5f8a8f92b3173ad5
m3llad0 <a01655451@tec.mx>
2023-10-19 17:22:00 -0600

    fixed footer

--- src/app/footer/footer.jsx
+++ src/app/footer/footer.jsx
@@ -5,7 +5,11 @@ export default function Footer() {
     <footer className="bg-gray-900 text-white py-3 text-center fixed bottom-0 w-full">
       <div className="container mx-auto">
       <hr className="border-gray-200 mx-auto dark:border-gray-700 w-2/4 my-2"/>
-        © 2023 <a href="https://github.com/No-Linger" className="hover:underline">No-Linger™</a>. Todos los derechos reservados. <a className="hover:underline">Términos y condiciones</a>.
+        <span data-testid = "footer-text" className="flex justify-center text-sm text-gray-500 sm:text-center dark:text-gray-400 space-x-5">
+              © 2023 <a href="https://github.com/No-Linger" className="hover:underline ml-1">No-Linger™.</a> 
+              <p>Todos los derechos reservados.</p> 
+              <a className="hover:underline"> Términos y condiciones.</a> 
+        </span>
       </div>
     </footer>
   );
