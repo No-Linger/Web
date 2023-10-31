a6918b5f624f030ca098f060f74447341d919ec8
m3llad0 <a01655451@tec.mx>
2023-10-31 17:06:08 -0600

    improved env handling

---
+++ src/app/utils/config.js
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+
+export const BUCKET_URL = process.env.BUCKET_URL;
+export const NEXT_PUBLIC_GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+export const NEXT_PUBLIC_GOOGLE_CLIENT_SECRET = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET;   
+export const NEXT_AUTH_URL = process.env.NEXT_AUTH_URL; 
+export const NEXT_AUTH_SECRET = process.env.NEXT_AUTH_SECRET; 
+export const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
