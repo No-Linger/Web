852af23f9e11b29677c25d2870261437ec9c8763
m3llad0 <a01655451@tec.mx>
2023-10-31 20:23:42 -0600

    make bucket url public

--- src/app/utils/config.js
+++ src/app/utils/config.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 
-export const BUCKET_URL = process.env.BUCKET_URL;
+export const NEXT_PUBLIC_BUCKET_URL = process.env.NEXT_PUBLIC_BUCKET_URL;
 export const NEXT_PUBLIC_GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 export const NEXT_PUBLIC_GOOGLE_CLIENT_SECRET = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET;   
 export const NEXT_AUTH_URL = process.env.NEXT_AUTH_URL; 
