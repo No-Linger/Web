1a9080c1e8b7e172f96153061a7da4c9d73adf78
m3llad0 <a01655451@tec.mx>
2023-11-25 22:31:29 -0600

    added authorization header in requests

--- src/app/tiendas/page.js
+++ src/app/tiendas/page.js
@@ -12,6 +12,7 @@ export default function Tiendas() {
   const router = useRouter()
   const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [token, setToken] = useState(null);
   const [tiendas, setTiendas] = useState({
     name: "",
     address: "",
@@ -38,17 +39,25 @@ export default function Tiendas() {
     setLoading(true)
   }
   else{
-    console.log(session)
+    setToken(session.accessToken)
     setLoading(false)
   }
 }
 
   useEffect(() => {
-
     checkSession()
-    axios.get(`${NEXT_PUBLIC_API_URL}/getStores`)
+  }, []);
+
+  useEffect(() => {
+    if (!loading && token) {
+      axios.get(`${NEXT_PUBLIC_API_URL}/getStores`, 
+      {
+        headers: {
+          'Authorization': token
+        }
+      })
       .then((response) => {
-        console.log(response.data.stores);
+        console.log(response.data);
         setTiendasList(response.data.stores);
         setLoading(false);
       })
@@ -56,7 +65,8 @@ export default function Tiendas() {
         console.log(error);
         setLoading(false);
       });
-  }, []);
+    }
+  }, [loading, token]);
 
   async function addTienda() {
     if (!tiendas.name || !tiendas.address || !tiendas.manager) {
@@ -66,11 +76,21 @@ export default function Tiendas() {
     }
 
     try {
-      const response = await axios.post(`${NEXT_PUBLIC_API_URL}/saveStore`, tiendas);
+      const response = await axios.post(
+        `${NEXT_PUBLIC_API_URL}/saveStore`,
+        tiendas, 
+        {
+          headers: { 'Authorization': token },
+        }
+      );
       console.log(response);
       setMessage({ ...message, message: 'Tienda agregada correctamente', status: true });
 
-      axios.get(`${NEXT_PUBLIC_API_URL}/getStores`)
+      axios.get(`${NEXT_PUBLIC_API_URL}/getStores`, {
+        headers: {
+          'Authorization': token
+        }
+      })
       .then((response) => {
         setTiendasList(response.data.stores);
       })
@@ -89,18 +109,16 @@ export default function Tiendas() {
 
   const columns = ['Nombre', 'Dirección', 'Encargado'];
 
-  console.log(tiendasList.map((tienda) => tienda));
-
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-700">
       <Navbar />
       {loading ? (
       <div className="flex items-center justify-center w-full h-screen">
           <div role="status">
-          <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
+          <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
           <span className="sr-only">Loading...</span>
       </div>
-</div>
+    </div>
 
       ) : (
         <main>
